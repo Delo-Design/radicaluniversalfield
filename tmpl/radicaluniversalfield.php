80aa85e38c30ca68ba631a15da1e4b949b732241
Dmitrii Cymbal <tsymbal.mitia@gmail.com>
2021-06-18 18:36:02 +0300

    обновление

--- tmpl/radicaluniversalfield.php
+++ tmpl/radicaluniversalfield.php
@@ -1,1 +1,13 @@
 <?php
+extract($displayData);
+
+$parse = json_decode($field->value, JSON_OBJECT_AS_ARRAY);
+
+if(is_array($parse))
+{
+	echo '<code>' . print_r($parse) . '</code>';
+}
+else
+{
+	echo $field->value;
+}
